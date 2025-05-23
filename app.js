3375894fcd38c762fd379aab09b6d1fead9766e3
Francis <brakay1136@gmail.com>
2025-05-22 18:47:13 -0700

    linked thumbnail image to product image

---
+++ app.js
@@ -0,0 +1,32 @@
+let overviewList = [
+    "images/image-product-1.jpg",
+    "images/image-product-2.jpg",
+    "images/image-product-3.jpg",
+    "images/image-product-4.jpg",
+]
+
+const overviewImg = document.querySelector('.shoe-img');
+overviewImg.src = overviewList[0];
+const thumbnails = document.querySelectorAll('.thumbnail')
+const shoesImgThumbnails = document.querySelector('.shoes-img-thumbnails')
+
+//link thumbnail image to product image
+function linkThumbnailToProductImg(e){
+     if(e.target.classList.contains('thumbnail')){
+        thumbnails.forEach((thumbnail, index) => {
+            if(e.target == thumbnail){
+                thumbnail.classList.add('active-thumbnail')
+                overviewImg.src = overviewList[index]
+            }else{
+                thumbnail.classList.remove('active-thumbnail')
+
+            }
+        })
+    
+    }
+}  
+
+shoesImgThumbnails.addEventListener('click', linkThumbnailToProductImg)
+
+
+
